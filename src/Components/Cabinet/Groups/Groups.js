b685b99c3e033d451dfb6222e786b4315bd8f8e6
khabibullo <saidakhmatov.k@gmail.com>
2024-03-25 14:22:33 +0500

    customized select

--- src/Components/Cabinet/Groups/Groups.js
+++ src/Components/Cabinet/Groups/Groups.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef, useLayoutEffect } from "react";
 import {
   FormControl,
   InputLabel,
@@ -12,6 +12,8 @@ import {
   SPACING_SM,
   SIDEBAR_OPEN_WIDTH,
   SPACING_MD,
+  FONT_BLUE_DARK,
+  FONT_SM,
 } from "../../../Constants/stylesConstants";
 import { ArrowDownwardOutlined } from "@mui/icons-material";
 
@@ -26,14 +28,23 @@ const Main = styled("div")({
   "& .flex": {
     display: "flex",
   },
+  "& .items-center": {
+    alignItems: "center",
+  },
   "& .justify-between": {
     justifyContent: "space-between",
+  },
+  "& .justify-around": {
+    justifyContent: "space-around",
   },
   "& .gap-sm": {
     gap: `${SPACING_SM}px`,
   },
   "& .gap-md": {
     gap: `${SPACING_SM}px`,
+  },
+  "& .full-width": {
+    width: "100%",
   },
 });
 
@@ -46,23 +57,33 @@ const Header = styled("div")({
 const Title = styled("h1")({
   margin: "0",
   color: "#1c274c",
-  fontSize: "2.8rem",
+  fontSize: "2.5rem",
   fontWeight: "1000",
 });
 
-const StyledSelect = styled(Select)({
-  width: "100%",
+const InputLabelStyled = styled(InputLabel)({
+  color: FONT_BLUE_DARK,
+  fontWeight: "600",
+  font: "inherit",
+  fontSize: `${FONT_SM}`,
+});
+
+const SelectStyled = styled(Select)({
+  width: "auto",
   backgroundColor: "white",
-  // padding: "15px",
-  border: "2px solid #E5E5E5",
   borderRadius: "12px",
-  fontSize: "28px",
+  font: "inherit",
+  fontSize: `${FONT_SM}`,
+  color: `${FONT_BLUE_DARK}`,
   "& .MuiSelect-select": {
     padding: "15px",
+    display: "flex",
+    alignItems: "center",
   },
-  "& *": {
-    border: "none",
-  },
+  "& .MuiOutlinedInput-notchedOutline, &:hover .MuiOutlinedInput-notchedOutline, &:focus .MuiOutlinedInput-notchedOutline":
+    {
+      border: "2px solid #E5E5E5 !important",
+    },
 });
 
 const Groups = () => {
@@ -71,26 +92,61 @@ const Groups = () => {
       <Main>
         <Header>
           <Title>Группы</Title>
-          <div className="flex justify-between gap-sm">
-            <FormControl fullWidth>
-              <InputLabel htmlFor="teacher-select">Преподаватель</InputLabel>
-              <StyledSelect
-                // value={state.age}
-                // onChange={handleChange}
-                inputProps={{
-                  name: "age",
-                  id: "teacher-select",
-                }}
+          <div className="flex items-center gap-sm">
+            <FormControl sx={{ minWidth: 200 }}>
+              <InputLabelStyled
+                htmlFor="teacher-select"
+                id="teacher-select-label"
               >
-                <MenuItem value={10}>10</MenuItem>
-                <MenuItem value={20}>20</MenuItem>
-                <MenuItem value={30}>30</MenuItem>
-              </StyledSelect>
+                Преподаватель
+              </InputLabelStyled>
+              <SelectStyled
+                labelId="teacher-select-label"
+                id="teacher-select"
+                label="Преподаватель"
+                autoWidth
+              >
+                <MenuItem value={1}>Eshmatov Toshmat</MenuItem>
+                <MenuItem value={2}>Aliyev Shohrux</MenuItem>
+                <MenuItem value={3}>Azizova Aziza</MenuItem>
+              </SelectStyled>
             </FormControl>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
-            <div>1</div>
+            <FormControl sx={{ minWidth: 100 }}>
+              <InputLabelStyled
+                htmlFor="teacher-select"
+                id="course-select-label"
+              >
+                Курс
+              </InputLabelStyled>
+              <SelectStyled
+                labelId="course-select-label"
+                id="course-select"
+                label="Курс"
+                autoWidth
+              >
+                <MenuItem value={1}>Eshmatov Toshmat</MenuItem>
+                <MenuItem value={2}>Aliyev Shohrux</MenuItem>
+                <MenuItem value={3}>Azizova Aziza</MenuItem>
+              </SelectStyled>
+            </FormControl>
+            <FormControl sx={{ minWidth: 120 }}>
+              <InputLabelStyled
+                htmlFor="teacher-select"
+                id="state-select-label"
+              >
+                Статус
+              </InputLabelStyled>
+              <SelectStyled
+                labelId="state-select-label"
+                id="state-select"
+                label="Статус"
+                autoWidth
+              >
+                <MenuItem value={1}>Eshmatov Toshmat</MenuItem>
+                <MenuItem value={2}>Aliyev Shohrux</MenuItem>
+                <MenuItem value={3}>Azizova Aziza</MenuItem>
+              </SelectStyled>
+            </FormControl>
           </div>
         </Header>
       </Main>
--- src/Constants/stylesConstants.js
+++ src/Constants/stylesConstants.js
@@ -1,3 +1,10 @@
 export const SIDEBAR_OPEN_WIDTH = 250;
 export const SPACING_SM = 20;
 export const SPACING_MD = 30;
+
+// colors
+export const FONT_BLUE_DARK = "#1C274C";
+
+// font sizes
+export const FONT_SM = "1.2rem";
+export const FONT_MD = "1.75rem";
