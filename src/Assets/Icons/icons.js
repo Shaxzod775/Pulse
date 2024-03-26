d9b76f88b6795c8b956f22cfe8a6c3ece6766e2f
khabibullo <saidakhmatov.k@gmail.com>
2024-03-26 14:04:06 +0500

    Started importing icons svgs, added grid, card in Group

---
+++ src/Assets/Icons/icons.js
@@ -0,0 +1,21 @@
+import { ReactComponent as ArrowD } from "./svgs/arrowDown.svg";
+import { ReactComponent as Edit } from "./svgs/edit.svg";
+import { ReactComponent as DateStart } from "./svgs/dateStart.svg";
+import { ReactComponent as DateEnd } from "./svgs/dateEnd.svg";
+import { ReactComponent as Notebook } from "./svgs/notebook.svg";
+import { ReactComponent as Clock } from "./svgs/clock.svg";
+import { ReactComponent as Calendar } from "./svgs/calendar.svg";
+import { ReactComponent as UsersGroupRounded } from "./svgs/usersGroupRounded.svg";
+import { ReactComponent as AcademicCap } from "./svgs/academicCap.svg";
+
+export const Icons = {
+  ArrowD,
+  Edit,
+  DateStart,
+  DateEnd,
+  Notebook,
+  Clock,
+  Calendar,
+  UsersGroupRounded,
+  AcademicCap,
+};
--- src/Components/Cabinet/Groups/Groups.js
+++ src/Components/Cabinet/Groups/Groups.js
@@ -1,15 +1,22 @@
 import React from "react";
 import {
   Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
   FormControl,
+  Grid,
   InputLabel,
   MenuItem,
   Paper,
   Select,
   ThemeProvider,
+  Typography,
   createTheme,
   styled,
 } from "@mui/material";
+import { Icons } from "../../../Assets/Icons/icons";
 
 const theme = createTheme({
   palette: {
@@ -25,9 +32,23 @@ const theme = createTheme({
       dark: "#1C274C",
       contrastText: "#00000",
     },
+    purpleGray: {
+      main: "#A2A2C2",
+      light: "#A2A2C2",
+      dark: "#A2A2C2",
+      contrastText: "#ffffff",
+    },
+    orange: {
+      main: "#F79B16",
+      light: "#FBF3E6",
+      dark: "#F79B16",
+      contrastText: "#ffffff",
+    },
   },
   typography: {
     fontSize: {
+      xxs: "0.8rem",
+      xs: "1rem",
       sm: "1.2rem",
       md: "1.75rem",
       lg: "2.5rem",
@@ -42,6 +63,9 @@ const theme = createTheme({
   custom: {
     sidebarOpenWidth: 250,
     spacing: {
+      x4s: 3,
+      x3s: 5,
+      xxs: 10,
       xs: 15,
       sm: 20,
       md2: 27,
@@ -54,14 +78,14 @@ const Root = styled("div")(({ theme }) => ({
   width: `calc(100vw - ${
     theme.custom.sidebarOpenWidth + theme.custom.spacing.sm * 2
   }px)`,
-}));
-
-const Main = styled("div")(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  gap: theme.custom.spacing.md,
   "& .flex": {
     display: "flex",
+  },
+  "& .flex-column": {
+    flexDirection: "column",
+  },
+  "& .flex-row": {
+    flexDirection: "row",
   },
   "& .items-center": {
     alignItems: "center",
@@ -71,6 +95,18 @@ const Main = styled("div")(({ theme }) => ({
   },
   "& .justify-around": {
     justifyContent: "space-around",
+  },
+  "& .gap-x4s": {
+    gap: theme.custom.spacing.x4s,
+  },
+  "& .gap-x3s": {
+    gap: theme.custom.spacing.x3s,
+  },
+  "& .gap-xxs": {
+    gap: theme.custom.spacing.xxs,
+  },
+  "& .gap-xs": {
+    gap: theme.custom.spacing.xs,
   },
   "& .gap-sm": {
     gap: theme.custom.spacing.sm,
@@ -83,11 +119,17 @@ const Main = styled("div")(({ theme }) => ({
   },
 }));
 
-const Header = styled("div")({
+const Main = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  gap: theme.custom.spacing.md,
+}));
+
+const Header = styled("div")(({ theme }) => ({
   display: "flex",
   justifyContent: "space-between",
   alignItems: "center",
-});
+}));
 
 const Title = styled("h1")(({ theme }) => ({
   margin: "0",
@@ -101,30 +143,82 @@ const InputLabelStyled = styled(InputLabel)(({ theme }) => ({
   fontWeight: "600",
   font: "inherit",
   fontSize: theme.typography.fontSize.sm,
+  lineHeight: theme.typography.fontSize.sm,
 }));
 
-const SelectStyled = styled(Select)({
+const SelectStyled = styled(Select)(({ theme }) => ({
   backgroundColor: "white",
   borderRadius: theme.shape.borderRadius,
   font: "inherit",
   fontSize: theme.typography.fontSize.sm,
+  lineHeight: theme.typography.fontSize.sm,
   color: theme.palette.darkBlue.main,
-  "& .MuiSelect-select": {
-    padding: theme.custom.spacing.xs,
-    display: "flex",
-    alignItems: "center",
+  display: "flex",
+  alignItems: "center",
+  "& .MuiSelect-select.MuiSelect-outlined.MuiInputBase-input.MuiOutlinedInput-input":
+    {
+      padding: theme.custom.spacing.xs,
+      paddingRight: "32px",
+      display: "flex",
+      alignItems: "center",
+      minHeight: theme.typography.fontSize.sm,
+      height: "auto",
+    },
+  "& .MuiSelect-icon.MuiSelect-iconOutlined": {
+    width: theme.typography.fontSize.sm,
+    height: "auto",
   },
   "& .MuiOutlinedInput-notchedOutline, &:hover .MuiOutlinedInput-notchedOutline, &:focus .MuiOutlinedInput-notchedOutline":
     {
       border: "2px solid #E5E5E5",
     },
-});
+}));
 
-const ButtonStyled = styled(Button)((props) => ({
+const ButtonStyled = styled(Button)(({ theme }) => ({
   borderRadius: theme.shape.borderRadius,
   font: "inherit",
   fontSize: theme.typography.fontSize.sm,
+  lineHeight: theme.typography.fontSize.sm,
   padding: theme.custom.spacing.xs,
+  textTransform: "capitalize",
+}));
+
+const CardButton = styled(Button)(({ theme }) => ({
+  width: "100%",
+  borderRadius: theme.shape.borderRadius,
+  font: "inherit",
+  fontSize: theme.typography.fontSize.xs,
+  lineHeight: theme.typography.fontSize.xs,
+  padding: theme.custom.spacing.xxs,
+  textTransform: "capitalize",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  gap: theme.custom.spacing.x3s,
+  "& svg": {
+    width: theme.typography.fontSize.xs,
+    height: "auto",
+  },
+}));
+
+const GroupsContainer = styled("div")(({ theme }) => ({}));
+
+const GroupCard = styled(Card)(({ theme }) => ({
+  borderRadius: theme.shape.borderRadius * 2,
+  padding: theme.custom.spacing.sm,
+  boxShadow: "none",
+  fontSize: theme.typography.fontSize.xs,
+  fontWeight: 600,
+}));
+
+const CardTag = styled("span")(({ theme, color = "orange" }) => ({
+  borderRadius: theme.custom.spacing.xs,
+  paddingTop: theme.custom.spacing.x3s,
+  paddingBottom: theme.custom.spacing.x3s,
+  paddingRight: theme.custom.spacing.xxs,
+  paddingLeft: theme.custom.spacing.xxs,
+  backgroundColor: theme.palette[color].light,
+  color: theme.palette[color].main,
 }));
 
 const Groups = () => {
@@ -147,6 +241,7 @@ const Groups = () => {
                   id="teacher-select"
                   label="Преподаватель"
                   autoWidth
+                  IconComponent={Icons.ArrowD}
                 >
                   <MenuItem value={1}>Eshmatov Toshmat</MenuItem>
                   <MenuItem value={2}>Aliyev Shohrux</MenuItem>
@@ -165,6 +260,7 @@ const Groups = () => {
                   id="course-select"
                   label="Курс"
                   autoWidth
+                  IconComponent={Icons.ArrowD}
                 >
                   <MenuItem value={1}>Eshmatov Toshmat</MenuItem>
                   <MenuItem value={2}>Aliyev Shohrux</MenuItem>
@@ -183,6 +279,7 @@ const Groups = () => {
                   id="state-select"
                   label="Статус"
                   autoWidth
+                  IconComponent={Icons.ArrowD}
                 >
                   <MenuItem value={1}>Eshmatov Toshmat</MenuItem>
                   <MenuItem value={2}>Aliyev Shohrux</MenuItem>
@@ -190,10 +287,82 @@ const Groups = () => {
                 </SelectStyled>
               </FormControl>
               <ButtonStyled variant="contained" color="aqua">
-                Boom +
+                Создать +
               </ButtonStyled>
             </div>
           </Header>
+          <GroupsContainer>
+            <Grid
+              container
+              justifyContent="center"
+              spacing={`${theme.custom.spacing.sm}px`}
+            >
+              <Grid item xs="auto" md="auto" lg={3}>
+                <GroupCard>
+                  <CardContent
+                    sx={{
+                      padding: 0,
+                      paddingBottom: `${theme.custom.spacing.md2}px`,
+                      color: theme.typography.color.darkBlue,
+                    }}
+                  >
+                    <div className="flex flex-column gap-sm">
+                      <div className="flex items-center justify-between">
+                        <Title
+                          sx={{
+                            fontSize: theme.typography.fontSize.sm,
+                          }}
+                        >
+                          GR011-62
+                        </Title>
+                        <CardTag>Набор</CardTag>
+                      </div>
+                      <div className="flex justify-between">
+                        <div className="flex items-center gap-x4s">
+                          <Icons.DateStart />
+                          <span>12.08.2022</span>
+                        </div>
+                        <div className="flex items-center gap-x4s">
+                          <Icons.DateEnd />
+                          <span>10.10.2022</span>
+                        </div>
+                      </div>
+                      <div className="flex flex-column gap-xs">
+                        <div className="flex justify-between">
+                          <div className="flex items-center gap-x4s">
+                            <Icons.Notebook />
+                            <span>Python</span>
+                          </div>
+                          <div className="flex items-center gap-x4s">
+                            <Icons.Clock />
+                            <span>14:00-16:00</span>
+                          </div>
+                        </div>
+                        <div className="flex items-center gap-x4s">
+                          <Icons.Calendar />
+                          <span>Пн, Ср, Пт</span>
+                        </div>
+                        <div className="flex items-center gap-x4s">
+                          <Icons.UsersGroupRounded />
+                          <span>8 человек</span>
+                        </div>
+                        <div className="flex items-center gap-x4s">
+                          <Icons.AcademicCap />
+                          <span>Филиппов Александр</span>
+                        </div>
+                      </div>
+                    </div>
+                  </CardContent>
+                  <CardActions sx={{ padding: 0 }}>
+                    <CardButton variant="contained" color="purpleGray">
+                      <Icons.Edit />
+                      <span>Редактировать</span>
+                    </CardButton>
+                  </CardActions>
+                </GroupCard>
+              </Grid>
+            </Grid>
+          </GroupsContainer>
         </Main>
       </Root>
     </ThemeProvider>
